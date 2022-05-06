4b6a6b6b41e30014d5aea705d63978c9833f1d0f
estevesryan <ryanplay71@gmail.com>
2022-05-06 20:19:03 -0300

    add mobine menu

--- aula-01/js/script.js
+++ aula-01/js/script.js
@@ -1,5 +1,15 @@
 addEventListener("scroll", fillHeader)
+openMenu.addEventListener("click", showMenu)
+closeMenu.addEventListener("click", exitMenu)
 
-function fillHeader (){
+function fillHeader() {
     scrollY > 30 ? header.setAttribute("class", "scroll") : header.removeAttribute("class", "scroll")
+}
+
+function showMenu() {
+    body.setAttribute("class", "menu-expended")
+}
+
+function exitMenu() {
+    body.removeAttribute("class", "menu-expended")
 }
